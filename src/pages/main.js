2dcb8988710bc6afcdfbaa6b7aa97c6326376d3a
gandy818 <gandy818@gmail.com>
2022-12-11 01:11:15 +0900

    Style : publishing auth page

--- src/pages/main.js
+++ src/pages/main.js
@@ -38,8 +38,8 @@ function Main(){
     setSignUpForPw(e.target.value);
   };
 
-  const signIn = () => {
-    Axios.post("/auth/signin", {
+  const signIn = async () => {
+   await Axios.post("/auth/signin", {
       signInForEmail, signInForPw
     }).then((res) => {
       console.log(res)
@@ -48,20 +48,32 @@ function Main(){
     })
   };
 
-  const signUp = async () => {
-    if (signUpValid) {
-      await Axios.post("/auth/signup", {
-        signUpForEmail, signUpForPw
-      }).then((res) => {
-        if(res.status === 201) {
-          alert("회원가입이 완료되었습니다.")
-          setSignUpForEmail("")
-          setSignUpForPw("")
-        }
-      }).catch((err) => {
-        console.log(err)
-      })
-    }
+  const signUp = () => {
+    axios.post("http://localhost:8000/auth/signup", {
+      signUpForEmail, signUpForPw
+    }, {
+      headers: {
+        "Content-Type": "application/json",
+      }
+    }).then((res) => {
+      console.log(res)
+    }).catch((err) => {
+      console.log(err)
+    })
+
+    // if (signUpValid) {
+    //   Axios.post("/auth/signup", {
+    //     signUpForEmail, signUpForPw
+    //   }).then((res) => {
+    //     if(res.status === 201) {
+    //       alert("회원가입이 완료되었습니다.")
+    //       setSignUpForEmail("")
+    //       setSignUpForPw("")
+    //     }
+    //   }).catch((err) => {
+    //     console.log(err)
+    //   })
+    // }
   }
 
   const CheckSignInValid = () => {
@@ -83,23 +95,31 @@ function Main(){
     <div className="main">
       <div className="mainWrapper">
         <div className="signinSection">
+          <div className="title">로그인</div>
+          <div className="subTitle">회원가입을 하셨다면-</div>
           <div>
             <div>이메일</div>
+            <div className="subTitle">이메일에는 @가 반드시 포함되어야 합니다.</div>
             <div><input type="text" value={signInForEmail} onChange={onChangedSignInEmail} onKeyUp={CheckSignInValid}></input></div>
           </div>
           <div>
             <div>비밀번호</div>
+            <div className="subTitle">비밀번호는 8자 이상이여야합니다.</div>
             <div><input type="password" value={signInForPw} onChange={onChangedSignInPw} onKeyUp={CheckSignInValid}></input></div>
           </div>
           <div onClick={signIn} className={"btn" + (signInValid ? " isActive" : " notActive")}>로그인 하기</div>
         </div>
         <div className="signupSection">
+          <div className="title">회원가입</div>
+          <div className="subTitle">아직 회원이 아니시라면- </div>
           <div>
             <div>이메일</div>
+            <div className="subTitle">이메일에는 @가 반드시 포함되어야 합니다.</div>
             <div><input type="text" value={signUpForEmail} onChange={onChangedSignUpEmail} onKeyUp={CheckSignUpValid}></input></div>
           </div>
           <div>
             <div>비밀번호</div>
+            <div className="subTitle">비밀번호는 8자 이상이여야합니다.</div>
             <div><input type="password" value={signUpForPw} onChange={onChangedSignUpPw} onKeyUp={CheckSignUpValid}></input></div>
           </div>
           <div onClick={signUp} className={"btn" + (signUpValid ? " isActive" : " notActive")}>회원가입하기</div>
--- src/pages/todo.js
+++ src/pages/todo.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styled/todo.scss"
 
 function Todo () {
+  const [todo, setTodo] = useState('공부하기');
+  const [readOnly, setReadOnly] = useState(true)
+
+  const getTodo = (e) => {
+    setTodo(e.target.value)
+  };
+
+  const modify = () => {
+    setReadOnly(false)
+  }
+
+
   return (
-    <div>투두 페이지입니다.</div>
+    <div className="todo">
+      <div className="header">TODO</div>
+      <div className="addTodo">
+        <input type="text"></input>
+        <div>추가하기</div>
+      </div>
+      <div className="todoList">
+        <div>1</div>
+        <div><input type="checkbox"></input></div>
+        <input className="" type="text" readOnly={readOnly} value={todo} onChange={getTodo}></input>
+        <div className="btn" onClick={modify}>수정</div>
+        <div className="btn">삭제</div>
+      </div>
+    </div>
   )
 }
 
