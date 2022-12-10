2cf2f92eb3210693006c560a62ba0703f3872c3c
gandy818 <gandy818@gmail.com>
2022-12-11 02:44:30 +0900

    Feat : createTodo, getTodo api 통신

--- src/pages/todo.js
+++ src/pages/todo.js
@@ -1,17 +1,46 @@
 import React, { useState } from "react";
+import axios from "axios"
 import "../styled/todo.scss"
 
 function Todo () {
   const [todo, setTodo] = useState('공부하기');
   const [readOnly, setReadOnly] = useState(true)
 
+  const Axios = axios.create({
+    baseURL: "http://localhost:8000/",
+    // baseURL: "https://pre-onboarding-selection-task.shop/",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + localStorage.getItem("access_token")
+    }
+  });
+
   const getTodo = (e) => {
     setTodo(e.target.value)
   };
 
-  const modify = () => {
+  const createTodo = () => {
+    Axios.post("/todos", {
+      todo : "string"
+    }).then((res) => {
+      console.log(res)
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
+
+  const modifyTodo = () => {
     setReadOnly(false)
   }
+
+
+  Axios.get("/todos")
+    .then((res) => {
+      console.log(res)
+  }).catch((err) => {
+    console.log(err)
+  })
+
 
 
   return (
@@ -19,13 +48,13 @@ function Todo () {
       <div className="header">TODO</div>
       <div className="addTodo">
         <input type="text"></input>
-        <div>추가하기</div>
+        <div onClick={createTodo}>추가하기</div>
       </div>
       <div className="todoList">
         <div>1</div>
         <div><input type="checkbox"></input></div>
         <input className="" type="text" readOnly={readOnly} value={todo} onChange={getTodo}></input>
-        <div className="btn" onClick={modify}>수정</div>
+        <div className="btn" onClick={modifyTodo}>수정</div>
         <div className="btn">삭제</div>
       </div>
     </div>
